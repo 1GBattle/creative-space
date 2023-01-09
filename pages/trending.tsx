d43069829fd7dbbedbfb39e82deaaaa59d91758e
Gary Battle <garybattle321@gmail.com>
2023-01-09 16:40:23 -0600

    started implementation of the trending page

--- pages/trending.tsx
+++ pages/trending.tsx
@@ -1,9 +1,29 @@
-import {NextPage} from "next"
+import { NextPage } from 'next'
+import styles from '../styles/Trending.module.css'
+import NavBarComponent from '../Components/Nav/NavBarComponent'
 
 const Trending: NextPage = () => {
-  return (
-    <div>Trending Page</div>
-  )
+	return (
+		<div className={styles.page__container}>
+			<NavBarComponent />
+
+			<section className={styles.content__container}>
+				<div className={styles.trending__categories}>
+					<h1 className={styles.trending__categories__title}>
+						Trending Categories
+					</h1>
+				</div>
+
+				<div className={styles.trending__spaces}>
+					<h1 className={styles.trending__spaces__title}>Trending Spaces</h1>
+				</div>
+
+				<div className={styles.trending__users}>
+					<h1 className={styles.trending__users__title}>Trending Users</h1>
+				</div>
+			</section>
+		</div>
+	)
 }
 
 export default Trending
